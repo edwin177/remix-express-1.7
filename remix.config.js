9f71f77aff174ff75a121b37eedf09d68d9e73be
Edwin Choi <8732768+edwin177@users.noreply.github.com>
2022-09-10 01:13:31 -0700

    Set server in remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  server: "server.js",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
